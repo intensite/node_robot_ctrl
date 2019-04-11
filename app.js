169ad59752a04f666bfba29a2ccbffa3413f3ee0
Stephen Remillard <sremillard@pciauto.com>
2019-04-11 00:02:07 -0400

    Added experiments with game controller.

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 const Gamecontroller = require('gamecontroller');
 const ctrl = new Gamecontroller('jesstech');
 
-ctrl.connect(function() {
+ctrl.connect(function () {
     console.log('Game On!');
 });
 
-ctrl.on('1:press', function() {
+ctrl.on('1:press', function () {
     console.log('1 was pressed');
 });
 
-ctrl.on('1:release', function() {
+ctrl.on('1:release', function () {
     console.log('1 was released');
 });
-ctrl.on('3:press', function() {
+ctrl.on('3:press', function () {
     console.log('3 was pressed');
 });
 
-ctrl.on('3:release', function() {
+ctrl.on('3:release', function () {
     console.log('3 was released');
 });
 
-ctrl.on('JOYL:move', function(o) {
+ctrl.on('JOYL:move', function (o) {
     console.log(o);
-  });
-ctrl.on('JOYR:move', function(o) {
+});
+ctrl.on('JOYR:move', function (o) {
     console.log(o);
-  });
+});
 
 
+ctrl.on('error', function () {
+    console.log('Controller could not be found');
+});
+
+ctrl.on('close', function () {
+    console.log('Closing the controller, nothing else to do.');
+});
 
 // To get the full parsed HID data stream, you can run
 
