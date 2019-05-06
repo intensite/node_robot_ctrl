0704dd4c1ae622f8447cb3a2e579acc1e1ab4065
Stephen Remillard <sremillard@pciauto.com>
2019-05-06 00:31:23 -0400

    Use the READLINE parser to read one line at a time from SerialPort.

--- robot_serial.js
+++ robot_serial.js
@@ -1,13 +1,13 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
+// const Readline = SerialPort.parsers.Readline;
 
 const COM_PORT = "COM8";
 const BAUD_RATE = 115200;
 
 // Setup COM port 
 const port = new SerialPort(COM_PORT, { baudRate: BAUD_RATE });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
 
 // The open event is always emitted
 port.on('open', function () {
@@ -15,14 +15,13 @@ port.on('open', function () {
     console.log("Com Port was opened...")
 });
 
-// Display incomming serial port data
-port.on('data', function (data) {
-    console.log('Data:', data.toString());
-});
+// Display incomming serial port data using the READLINE parser
+parser.on('data', console.log)
 
 // Open errors will be emitted as an error event
 port.on('error', function (err) {
     console.log('Error: ', err.message)
 });
 
-module.exports = port;
+module.exports.port = port;
+module.exports.parser = parser;
