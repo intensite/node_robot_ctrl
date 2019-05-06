3e1a752a2f69ebe38658605ea95a34fd5f7a16b4
Stephen Remillard <sremillard@pciauto.com>
2019-05-06 00:34:32 -0400

    Altered the getPosition() method to return a promise. It also uses the SerialPort parser to read the full line.

--- robot_command.js
+++ robot_command.js
@@ -1,4 +1,3 @@
-
 const STEPS_DISTANCE = 1;       // Distance of each steps (in mm)
 const STEPS_SPEED = 30;         // Speed of the stepper moves (in mm/s)
 const DIST_AND_SPEED = STEPS_DISTANCE + " F" + STEPS_SPEED + "\r";
@@ -12,8 +11,9 @@ const DIST_AND_SPEED = STEPS_DISTANCE + " F" + STEPS_SPEED + "\r";
 
 class Robot {
 
-    constructor(port) {
-        this.port = port;
+    constructor(serialPort) {
+        this.port = serialPort.port;
+        this.parser = serialPort.parser;
     }
 
     /**********************************************
@@ -57,15 +57,15 @@ class Robot {
         // Relative move single axis
 
         this.port.write('G91\r')  // Set to RELATIVE coordinate mode
-        
-        switch(m.axis){
+
+        switch (m.axis) {
             case 'X':
                 this.port.write(`G1 X${m.value} F${m.speed}\r`);
                 break;
-                case 'Y':
+            case 'Y':
                 this.port.write(`G1 Y${m.value} F${m.speed}\r`);
                 break;
-                case 'Z':
+            case 'Z':
                 this.port.write(`G1 Z${m.value} F${m.speed}\r`);
                 break;
             default:
@@ -73,21 +73,38 @@ class Robot {
 
     }
     absoluteMove(m) {
-        // Absolute move single axis
+        // Absolute move multiple axis
 
         this.port.write('G90\r')  // Set to ABSOLUTE coordinate mode
-        var gcode = 'G1' + (m.X ? ' X' + m.X : '' ) + (m.Y ? ' Y' + m.Y : '' ) + (m.Z ? ' Z' + m.Z : '' ) + (m.speed ? ' F' + m.speed : '' );
+        var gcode = 'G1' + (m.X ? ' X' + m.X : '') + (m.Y ? ' Y' + m.Y : '') + (m.Z ? ' Z' + m.Z : '') + (m.speed ? ' F' + m.speed : '');
 
         this.port.write(`${gcode}\r`);
-        this.getPosition(); // Return the current positions of all axis 
+        // this.getPosition(); // Return the current positions of all axis 
     }
 
     // Gripper functions
     openGripper() { this.port.write("M3\r") }
     closeGripper() { this.port.write("M5\r") }
     powerUpSteppers() { this.port.write("M17\r") }
-    powerDownSteppers() { this.port.write("M18\r")}
-    getPosition() { this.port.write("M114\r")} // Return the current positions of all axis 
+    powerDownSteppers() { this.port.write("M18\r") }
+    
+    getPosition() { 
+    // Return the current positions of all axis 
+    var positionInfo = "";
+
+    return new Promise((resolve, reject)=>{
+
+        this.port.write("M114\r"); 
+
+        this.parser.on('data', function (data) {
+            if(data.startsWith("C: ")){
+                positionInfo = data;
+                resolve(positionInfo);
+                // console.log('info:', data);
+            }
+        });
+     })
+    } 
 
 }
 
